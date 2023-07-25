4d36ae06e3f68037374139666371ba05f94e59a9
filippo199600 <pippotartaglia@gmail.com>
2023-07-25 16:48:31 +0200

    Feat cycle solution

--- JSnack2/js/main.js
+++ JSnack2/js/main.js
@@ -6,15 +6,19 @@ const listName = ["Mario", "Angela", "Stefano", "Gianmarco", "Thomas"];
 let surnameName;
 
 // COMBINO LE 2 LISTE
-for (i = 0; i < listSurname.length; i++) {
-  surnameName = listName[0] + listSurname[i];
-  console.log(surnameName);
-  surnameName = listName[1] + listSurname[i];
-  console.log(surnameName);
-  surnameName = listName[2] + listSurname[i];
-  console.log(surnameName);
-  surnameName = listName[3] + listSurname[i];
-  console.log(surnameName);
-  surnameName = listName[4] + listSurname[i];
-  console.log(surnameName);
+for (let i = 0; i < listSurname.length; i++) {
+  for (let j = 0; j < listName.length; j++) {
+    surnameName = listName[j] + listSurname[i];
+    console.log(surnameName);
+  }
+  // surnameName = listName[0] + listSurname[i];
+  // console.log(surnameName);
+  // surnameName = listName[1] + listSurname[i];
+  // console.log(surnameName);
+  // surnameName = listName[2] + listSurname[i];
+  // console.log(surnameName);
+  // surnameName = listName[3] + listSurname[i];
+  // console.log(surnameName);
+  // surnameName = listName[4] + listSurname[i];
+  // console.log(surnameName);
 }
