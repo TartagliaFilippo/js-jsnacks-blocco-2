3c0738feb639c3c86004576a7fc1c6e7d4efd35f
filippo199600 <pippotartaglia@gmail.com>
2023-07-25 16:58:34 +0200

    Feat solution on screen

--- JSnack2/js/main.js
+++ JSnack2/js/main.js
@@ -2,6 +2,9 @@
 const listSurname = ["Rossi", "Baggio", "Tartaglia", "Parolin", "Alberti"];
 const listName = ["Mario", "Angela", "Stefano", "Gianmarco", "Thomas"];
 
+// LA MIA VARIABILE A SCHERMO
+const nameSurname = document.getElementById("name-surname");
+
 // CREO UNA VARIABILE CHE MI VA A CREARE LE VARIE COMBINAZIONI
 let surnameName;
 
@@ -10,6 +13,8 @@ for (let i = 0; i < listSurname.length; i++) {
   for (let j = 0; j < listName.length; j++) {
     surnameName = listName[j] + listSurname[i];
     console.log(surnameName);
+
+    nameSurname.innerHTML += surnameName + ` </br>`;
   }
   // surnameName = listName[0] + listSurname[i];
   // console.log(surnameName);
